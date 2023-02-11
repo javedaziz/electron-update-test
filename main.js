c204a2812d912f5b8d767b2ea59d07f3e1cb0281
Javed Aziz <>
2023-02-12 02:36:49 +1100

    2.0

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    backgroundColor:"red",
     webPreferences: {
       nodeIntegration: true,
     },
